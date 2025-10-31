d2d107e6c532ae65885b1554cb81deb748b24933
amine <amineotako64@gmail.com>
2025-10-31 09:57:13 +0100

    Changes to be committed: 	modified:   producer/produce_messages.py

--- producer/produce_messages.py
+++ producer/produce_messages.py
@@ -1,12 +1,11 @@
 import time
 from display_logging import logging
-from metrics_mon import start_http_server,record_messages_producer
+
 
 
 def produce_messages(
     SYMBOLS, fetch_quotes, send_messages, BASE_URL, api_key, qoute_topic
 ):
-    start_time=time.time()
     logging.info(".........Starting the data pipeline.........")
     while True:
         for symbol in SYMBOLS:
@@ -14,5 +13,4 @@ def produce_messages(
             if quote:
                 logging.info(f"Producing:{quote}")
                 send_messages(value=quote, topic=qoute_topic)
-                record_messages_producer(start_time)
         time.sleep(6)
